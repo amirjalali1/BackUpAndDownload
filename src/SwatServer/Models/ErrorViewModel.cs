99627b8047f58bebaabf95dbbc31679fba984a58
amir jalali <amir.jalali@gmail.com>
2020-10-27 13:40:40 +0330

    add swat server

---
+++ src/SwatServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SwatServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
